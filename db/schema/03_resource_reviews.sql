b68d8cf598c03016bb9cf0251e1d92d0e7b072ee
Amphakarn <pisuthigomol@yahoo.com>
2021-01-09 21:26:15 +0000

    Changed isLiked to like in 03_resource_reviews table

--- db/schema/03_resource_reviews.sql
+++ db/schema/03_resource_reviews.sql
@@ -5,5 +5,5 @@ CREATE TABLE resource_reviews (
   user_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
   resource_id INTEGER REFERENCES resources(id) ON DELETE CASCADE,
   rating SMALLINT DEFAULT 0,
-  isLiked BOOLEAN DEFAULT false
+  like BOOLEAN DEFAULT false
 );
