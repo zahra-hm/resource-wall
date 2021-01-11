c82a02c96f974725f04ae3f26f9defde01d8223e
Amphakarn <pisuthigomol@yahoo.com>
2021-01-11 22:55:58 +0000

    Update users schema for image_url field

--- db/schema/01_users.sql
+++ db/schema/01_users.sql
@@ -5,7 +5,7 @@ CREATE TABLE users (
   username VARCHAR(255) NOT NULL,
   email VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  image_url TEXT
+  image_url TEXT DEFAULT 'https://icon-library.net/images/generic-user-icon/generic-user-icon-10.jpg'
 );
 
 
