6c775e155a2dbba401e44c45cdd84c3b9ea589f7
zahra-hm <zahra520@hotmail.com>
2021-01-12 18:55:11 +0000

    changed repeating variable names to be unique to avoid errors

--- routes/database-functions-test.js
+++ routes/database-functions-test.js
@@ -10,7 +10,7 @@ const { getUserByID, getAllResources, getUserResourcesByUserID, getUserByEmail, 
 
 // // Test getUserByID
 // // SEED DATA: ____________ ;
-/*const user_id = 3;
+const user_id = 3;
 
 // callfunction and provide valid seed data.
 getUserByID(user_id).then(res => {
@@ -23,7 +23,7 @@ getUserByID(user_id).then(res => {
 
 // Test getResourcesByCategory /////////////////////////////////////////
 
-/*
+
 let category_id = 1;
 getResourcesByCategory(category_id).then(res => {
 
@@ -31,39 +31,32 @@ getResourcesByCategory(category_id).then(res => {
   console.log(res);
 
 }).catch(err => console.log(err));
-*/
 
 
-/*
+
+
 // TEST getSpecificCategoryInfo function ///////////////////////////////////////////////
 
-let category_id = 5;
-getSpecificCategoryInfo(category_id).then(res => {
+let category_id2 = 5;
+getSpecificCategoryInfo(category_id2).then(res => {
 
   console.log("output from getResourcesByCategory Query is: ")
   console.log(res);
 
 }).catch(err => console.log(err));
-*/
 
-/*
+
+
 // TEST getIsLikeValue function ///////////////////////////////////////////////
 let resource_id = 9;
-user_id = 12;
+user_id2 = 12;
 
-getIsLikeValue(user_id,resource_id).then(res => {
+getIsLikeValue(user_id2,resource_id).then(res => {
 
   console.log("output from getIsLikeValue Query is: ")
   console.log(res);
 
 }).catch(err => console.log(err));
-*/
-
-// TEST setIsLikeValue function ///////////////////////////////////////////////
-/*
-let user_id = 9;
-let resource_id = 5;
-let isLikeValue = true;
 
 // Test getUserByEmail
 
@@ -100,9 +93,9 @@ getCommentsForSpecificResource(resourceID2).then(res => {
 }).catch(err => console.log(err));
 
 
-const user_id = 3;
+const user_id3 = 3;
 // getResourceReviewsByOwnerID
-getResourceReviewsByOwnerID(user_id).then(res => {
+getResourceReviewsByOwnerID(user_id3).then(res => {
 
     console.log("output from getResourceReviewsByOwnerID Query is: ")
     console.log(res);
@@ -113,13 +106,13 @@ getResourceReviewsByOwnerID(user_id).then(res => {
 // const resourceID2 = 2;
 
 const owner_id = 5;
-const category_id = 2;
+const category_id3 = 2;
 const title = 'The Big Bang!';
 const url = 'https://en.wikipedia.org/wiki/Big_Bang';
 const description = 'The Big Bang theory is a cosmological model of the observable universe from the earliest known periods through its subsequent large-scale evolution';
 const created_at = '2019-04-23 09:09:09';
 
-addNewResources(owner_id, category_id, title, url, description, created_at).then(res => {
+addNewResources(owner_id, category_id3, title, url, description, created_at).then(res => {
 
   console.log("output from addNewResources Query is: ")
   console.log(res);
@@ -129,40 +122,51 @@ addNewResources(owner_id, category_id, title, url, description, created_at).then
 
 // Test addNewReviewIsRating
 
-const user_id3 =  3;
-const resource_id = 2;
+const user_id9 =  3;
+const resource_id2 = 2;
 const rating = 3;
 
-addNewReviewIsRating(user_id3, resource_id, rating).then(res => {
+addNewReviewIsRating(user_id9, resource_id2, rating).then(res => {
 
   console.log("output from addNewReviewIsRating Query is: ")
   console.log(res);
 
 }).catch(err => console.log(err));
-setIsLikeValue(user_id,resource_id, isLikeValue).then(res => {
+
+// TEST setIsLikeValue function ///////////////////////////////////////////////
+
+let user_id4 = 9;
+let resource_id3 = 5;
+let isLikeValue = true;
+
+setIsLikeValue(user_id4,resource_id3, isLikeValue).then(res => {
 
   console.log("output from setIsLikeValue Query is: ")
   console.log(res);
 
 }).catch(err => console.log(err));
-*/
+
 
 
 // TEST addNewReviewsIsLike function ///////////////////////////////////////////////
-/*
-let user_id = 11;
-let resource_id = 6;
-let isLikeValue = false;
 
-addNewReviewsIsLike(user_id,resource_id, isLikeValue).then(res => {
+let user_id5 = 11;
+let resource_id4 = 6;
+let isLikeValue2 = false;
+
+addNewReviewsIsLike(user_id5,resource_id4, isLikeValue2).then(res => {
 
   console.log("output from addNewReviewsIsLike Query is: ")
   console.log(res);
 }).catch(err => console.log(err));
 
-const resource_id = 4;
+
+
 // getResourceReviewsByResourceID
-getResourceReviewsByResourceID(resource_id).then(res => {
+
+const resource_id5 = 4;
+
+getResourceReviewsByResourceID(resource_id5).then(res => {
 
   console.log("output from getResourceReviewsByResourceID Query is: ")
   console.log(res);
@@ -184,12 +188,12 @@ addNewUser(username, email, password).then(res => {
 
 
 // Test addNewComment
-const user_id = 3;
-const resource_id = 3;
-const created_at = '2020-07-06T04:05:06.000Z';
+const user_id6 = 3;
+const resource_id6 = 3;
+const created_at2 = '2020-07-06T04:05:06.000Z';
 const comment = 'Porta lorem mollis aliquam ut porttitor leo a. Tristique sollicitudin nibh sit amet commodo. Feugiat sed lectus vestibulum mattis ullamcorper velit sed ullamcorper morbi.';
 
-addNewComment(user_id, resource_id, created_at, comment).then(res => {
+addNewComment(user_id6, resource_id6, created_at2, comment).then(res => {
 
   console.log("output from addNewComment Query is: ")
   console.log(res);
@@ -200,16 +204,16 @@ addNewComment(user_id, resource_id, created_at, comment).then(res => {
 
 // TEST updateUserName function ///////////////////////////////////////////////
 
-/*
 
-let user_id = 11;
-let username = 'jordan';
 
-updateUserName(user_id, username).then(res => {
+let user_id7 = 11;
+let username2 = 'jordan';
+
+updateUserName(user_id7, username2).then(res => {
 
   console.log("output from updateUserName Query is: ")
   console.log(res);
 
 }).catch(err => console.log(err));
 
-*/
+
