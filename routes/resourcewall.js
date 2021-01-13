24f2b7b28d73a66db17034203f057836f7109b80
Amphakarn <pisuthigomol@yahoo.com>
2021-01-13 20:41:37 +0000

    Created login route

--- routes/resourcewall.js
+++ routes/resourcewall.js
@@ -27,14 +27,103 @@ router.get("/logout", (req, res) => {
 
 
 
+//1 GET /resourcewall/login  
+// Email for testing: michaelgray@mail.com
+// Password for testing: $2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.
+router.get("/login", (req, res) => {
+  
+
+  const user_id = req.session.user_id;
+
+  if (user_id) {
+
+    res.redirect("/resources");
+
+  } else {
+
+    const templateVars = {};
+    res.render("login", templateVars);
+
+  }
+
+});
 
 
 
+//1 POST /resourcewall/login
+
+router.post("/login", (req, res) => {
+
+  const user_email = req.body.email;
+  const password = req.body.password;
+
+  databaseHelper.getUserByEmail(user_email)
+  .then(result => {
+
+    if (result && result.password === password) {
+
+      req.session.user_id = result.id;
+      res.redirect("/resources");
+
+    } else {
+
+      res.send("Your email or password is invalid");
+
+    }
+  })
+
+  .catch(error => {
+      console.log(err);
+  
+  });
 
 
+  // if (password === user.password) {
+  //   res.redirect("/resources");
+  // }
 
+// // Check if user has cookie id
+  // if (user_id) {
+
+  //   let user_email;
+
+  //   databaseHelper.getUserByID(user_id)
+  //   .then(result => {
+
+  //   // get user email from the users table
+  //     user_email = result.email;
+
+  //     console.log("result is: ", result);
+  //     console.log("user_email is: ", user_email);
+
+  //     if (user_email) {
+
+  //       res.redirect("/resources");
+
+  //     } else {
+
+  //       const templateVars = { 
+  //         user_email: user_email
+  //       };
+        
+  //       res.render("login", templateVars);
+
+  //     }
+  //   })
+  //   .catch(error => {
+  //     console.log(error);
+  //   })
+  // }
+
+
+});
+
+// Check if user is logged-in, re-route to Main Resources page
+
+// Else 
 
 
 
 
 module.exports = router;
+
