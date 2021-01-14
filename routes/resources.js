17b6eb4adf8c9bc99f0395c356a792bd5c1f0cd9
Amphakarn <pisuthigomol@yahoo.com>
2021-01-14 04:01:27 +0000

    Created GET and POST routes for add new resource

--- routes/resources.js
+++ routes/resources.js
@@ -37,7 +37,7 @@ router.get("/", (req, res) => {
 
       //res.send(result);
 
-      console.log('result is: ', result);
+      // console.log('result is: ', result);
       // get all resources from the user
       return databaseHelper.getAllResourcesAvgRating();
 
@@ -45,13 +45,13 @@ router.get("/", (req, res) => {
 
     }).then(result => {
 
-      console.log("Result is: ", result);
+      // console.log("Result is: ", result);
       //let avgRating = result;
       let allResources = result;
 
       //resourceDate = allResources.created_at;
 
-      console.log("resourceDate is: ", resourceDate);
+      // console.log("resourceDate is: ", resourceDate);
       let templateVars = {
         user_email: user_email,
         allResources: allResources
@@ -75,6 +75,97 @@ router.get("/", (req, res) => {
   */
 
 });
+
+
+// 1	GET	/resources/new
+// Show Add New Resource page only for loggin-in user
+
+router.get("/resources/new", (req, res) => {
+
+  let user_email;
+
+  // Check if user is logged-in, otherwise redirect to index page
+  const user_id = req.session.user_id;
+
+  if (user_id) {
+
+    databaseHelper.getUserByID(user_id)
+    .then(result => {
+
+      user_email = result.email;
+
+    const templateVars = { user_email: user_email};
+    res.render("new_resource", templateVars);
+    })
+
+  } else {
+
+    res.send("Please login or register to view requested page");
+    res.redirect("/index");
+
+  }
+
+});
+
+
+// 1	POST	/resources
+// Only logged-in user can add new resource
+
+router.post("/", (req, res) => {
+
+  let user_email;
+  const title = req.body.title;
+  const category_id = req.body.category_id;
+  const url = req.body.url;
+  const description = req.body.description;
+
+  const user_id = req.session.user_id;
+  const owner_id = user_id;
+  if (user_id) {
+
+    databaseHelper.getUserByID(user_id)
+    .then(result => {
+      user_email = result.email;
+
+      if (result) {
+        // console.log('Result from getUserByID is: ', result);
+        const result2 = databaseHelper.addNewResources(
+                owner_id,
+                category_id,
+                title,
+                url,
+                description
+                );
+          return result2;
+
+      }
+      // console.log("******* RESULT IS UNDEFINED WHY????????????????");
+    })
+    .then(result => {
+      // console.log('Result addNewResources is: ', result);
+      const newResource = result;
+
+      let templateVars = {
+        user_email: user_email,
+        allResources: newResource
+
+      };
+
+      res.render("main_resource", templateVars);
+
+    })
+  }
+
+})
+
+
+// 1  GET  /resources/:resource_id
+
+
+
+
+
+
 
 /////////////////////////////////////////////////////////
 // GET	/categories/:category_name/resources	show resources filtered by category	chosen category from pull down menu in nav bar
